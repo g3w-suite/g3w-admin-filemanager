0e1dd52b76b29c513a3ff57b72d757346079c668
Walter Lorenzetti <lorenzetti@gis3w.it>
2018-05-18 18:07:58 +0200

    cu

--- urls.py
+++ urls.py
@@ -2,13 +2,6 @@ from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 from .views import *
 
-'''
-   
-   url(r'^languages/(?P<file_js>[-_\.\\\w\d]+)$', login_required(FilemanagerServeLanguageView.as_view()),
-       name='filemanager-serve-file-config'),
-   url(r'^scripts/(?P<file>[-_\.\/\w\d]+)$', login_required(FilemanagerServeScriptView.as_view()),
-       name='filemanager-serve-file-config'),
-   '''
 
 urlpatterns = [
     url(r'^$', login_required(FilemanagerView.as_view()), name='filemanager-home'),
--- views.py
+++ views.py
@@ -20,6 +20,10 @@ class FilemanagerView(TemplateView):
 
 class FilemanagerServeConfigView(View):
 
+    @method_decorator(user_passes_test_or_403(lambda u: u.is_superuser))
+    def dispatch(self, *args, **kwargs):
+        return super(FilemanagerServeConfigView, self).dispatch(*args, **kwargs)
+
     def get(self, request, *args, **kwargs):
 
         path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static/RichFilemanager/config/')
@@ -30,30 +34,6 @@ class FilemanagerServeConfigView(View):
         # try to change language
         return JsonResponse(config)
 
-'''
-class FilemanagerServeLanguageView(View):
-
-    def get(self, request, *args, **kwargs):
-
-        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static/RichFilemanager/languages/')
-        file = open('{}{}'.format(path, kwargs['file_js']), 'r')
-        return JsonResponse(json.load(file))
-
-
-
-class FilemanagerServeScriptView(View):
-
-    def get(self, request, *args, **kwargs):
-
-        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static/RichFilemanager/scripts/')
-        file_js = kwargs['file']
-        file = open('{}{}'.format(path, file_js), 'r')
-
-        if file_js.endswith('.html'):
-            return HttpResponse(file.read())
-        else:
-            return JsonResponse(json.load(file))
-'''
 
 @csrf_exempt
 @user_passes_test_or_403(lambda u: u.is_superuser)
